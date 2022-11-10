6ec3d6651eebe2a61b4deef4241764ba5f22574d
hanna <hanna@hanna.lol>
2022-11-10 01:10:08 -0500

    refactor: register fastify websocket middleware

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { fastify } from 'fastify';
 import { loadConfig } from './config.js';
 
 const cfg = await loadConfig(process.env.CONFIG);
-const app = fastify({ logger: cfg.logging, bodyLimit: cfg.maxSize });
+const app = fastify({ logger: cfg.logging, bodyLimit: cfg.limits?.api });
 await app.register(import('@fastify/compress'), { encodings: ['gzip'] });
+await app.register(import('@fastify/websocket'), {
+  options: { maxPayload: cfg.limits?.gateway },
+});
 
 app.get('/', async (_, res) => res.status(200).send('hello world'));
 await app.listen({ port: cfg.port });
