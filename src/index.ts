795759e8fa212f3e7250b7cc0a2abc9587b80d6e
hanna <hanna@hanna.lol>
2022-11-10 00:55:59 -0500

    refactor: enable fastify compression with gzip support

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import { loadConfig } from './config.js';
 
 const cfg = await loadConfig(process.env.CONFIG);
 const app = fastify({ logger: cfg.logging, bodyLimit: cfg.maxSize });
+await app.register(import('@fastify/compress'), { encodings: ['gzip'] });
+
 app.get('/', async (_, res) => res.status(200).send('hello world'));
 await app.listen({ port: cfg.port });
