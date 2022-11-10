655a9063fb4172b292ed7b2ea0b89900209795b6
hanna <hanna@hanna.lol>
2022-11-10 00:38:15 -0500

    feat: initial fastify "hello world" server w/ json5 config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello world!');
+import { fastify } from 'fastify';
+import { loadConfig } from './config.js';
+
+const cfg = await loadConfig(process.env.CONFIG);
+const app = fastify({ logger: cfg.logging, bodyLimit: cfg.maxSize });
+app.get('/', async (_, res) => res.status(200).send('hello world'));
+await app.listen({ port: cfg.port });
